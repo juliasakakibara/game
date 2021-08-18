f497c4fd58771cf8a4ffaed0edd96e6a5a75da8c
juliasakakibara <juliasakakibara@gmail.com>
2021-08-17 22:12:55 -0300

    virus resolvido, vacina a resolver

--- js/index.js
+++ js/index.js
@@ -17,49 +17,35 @@ const context = canvas.getContext('2d');
 
 
 // set up player
-const player =  {
-  speed: 30, // px per second
-  x: 290, // position in x
-  y: 137, //position in y
-  r: 50 / 2, // radius in px
-  a: 90 / 180 * Math.PI, //players angle for shooting
-  width: 37, // players width
-  height: 50, // players height
-  canShoot: true,
-  handSanitizer: [],
-  infectionTime: 0,
-} 
+let player = newPlayer();
 
-// const player = Player.prototype;
-
-// class Player {
-  //   constructor () {
-  // create new player after getting contaminated                                                                
-//   this.speed = 30; // px per second
-//   this.x = 290; // position in x
-//   this.y = 137; //position in y
-//   this.r = 50 / 2; // radius in px
-//   this.a = 90 / 180 * Math.PI; //players angle for shooting
-//   this.width = 37; // players width
-//   this.height = 50; // players height
-//   this.canShoot = true;
-//   this.handSanitizer = [];
-//   this.infectionTime = 0;
-//   } 
-// }  
-
+function newPlayer() {
+  return {
+    speed: 30, // px per second
+    x: 290, // position in x
+    y: 137, //position in y
+    r: 50 / 2, // radius in px
+    a: 90 / 180 * Math.PI, //players angle for shooting
+    width: 37, // players width
+    height: 50, // players height
+    canShoot: true,
+    handSanitizer: [],
+    infectionTime: 0,
+    vaccination: [],
+  }
+}
   
 // set up virus
-const virus = {
-  x: Math.random() * canvas.width,
-  y: Math.random() * canvas.height,
-  xv: Math.floor(Math.random() * 50 + 100) / fps, // random speed between 100 and 150
-  yv: Math.floor(Math.random() * 50 + 100) / fps,
-  r: virusSize / 2,
-  size: 15,
-  speed: 10,
-};
-
+// const virus = {
+//   x: Math.random() * canvas.width,
+//   y: Math.random() * canvas.height,
+//   xv: Math.floor(Math.random() * 50 + 100) / fps, // random speed between 100 and 150
+//   yv: Math.floor(Math.random() * 50 + 100) / fps,
+//   r: virusSize / 2,
+//   size: 15,
+//   speed: 10,
+// };
+ 
 let viruses = [];
 createVirus();
 
@@ -67,8 +53,8 @@ function createVirus() {
   viruses = [];
   let x,y;
   for (let i = 0; i < virusNum; i++){
-    x: Math.floor(Math.random() * canvas.width);
-    y: Math.floor(Math.random() * canvas.height);
+    x= Math.floor(Math.random() * canvas.width);
+    y= Math.floor(Math.random() * canvas.height);
     viruses.push(newVirus(x,y));
   }
 }
@@ -81,6 +67,9 @@ function newVirus(x,y) {
     yv: Math.random() * virusSpeed / fps * Math.random() < 0.5 ? 1 : -1,
     r: virusSize / 2,
     a: Math.random() * Math.PI * 2, //in radians
+    r: virusSize / 2,
+    size: 15,
+    speed: 10,
  }
  return virus
 };
@@ -95,17 +84,18 @@ const vaccine = {
 
 //set up hand sanitazer
 const handSanitizers = {
-  shoot : 10, // max shoots on screen
+  shoot : 10, // max on screen
   speed: 12,
+
 };
 
 function shootHandSanitizer() {
   //create hand sanitizer
   if (player.canShoot && player.handSanitizer.length < handSanitizers.shoot) {
     player.handSanitizer.push({
-      x: player.x + player.width / 2, //posicao do shot
+      x: player.x + player.width / 2, //posicao do tiro
       y: player.y,
-      xv: handSanitizers.speed * Math.cos(player.a),
+      xv: handSanitizers.speed * Math.cos(player.a), 
       yv: -handSanitizers.speed * Math.sin(player.a),
     })
   }
@@ -152,6 +142,24 @@ function infection() {
   context.fill();
 };
 
+function vaccination() {
+  context.font='30px VT323';
+  context.fillStyle='#2B2B2B';
+  context.fillText(`${player.vaccination}ST DOSE`,120, 50);
+    
+  //testing boundaries
+  context.fillStyle='red';
+  context.beginPath();
+  context.rect(player.x, player.y, player.width, player.height);
+  context.fill();
+};
+
+
+
+
+console.log(viruses)
+
+
 
 function update() {
 
@@ -165,7 +173,9 @@ function update() {
         image.onload = (e) => {context.drawImage(image, player.x, player.y, player.width, player.height)} 
         }
       drawPlayer1();
-      } // else {blink and return to the staring position};
+      } else { // else {blink and return to the staring position};
+        player = newPlayer();
+      } 
     
 
     //clear canvas
@@ -180,12 +190,11 @@ function update() {
     } else if (player.x > canvas.width + player.r) {
         player.x = 0 - player.r;
     }
-
     //draw viruses
     for(let i = 0; i < viruses.length; i ++) {
       
         //draw virus
-      const drawVirus = () => {
+      const drawVirus = (virus) => {
         // const virus = {
         //   x: Math.random() * canvas.width,
         //   y: Math.random() * canvas.height,
@@ -195,26 +204,28 @@ function update() {
         image.src = '../img/coronga.svg'
         image.onload = (e) => {context.drawImage(image, virus.x, virus.y, virus.size, virus.size)}
       };
-      drawVirus();
-    }
+      drawVirus(viruses[i]);
+    
 
     //move the virus
     if(!colliding){
-      virus.x += virus.xv;
-      virus.y += virus.yv;
+      viruses[i].x += viruses[i].xv;
+      viruses[i].y += viruses[i].yv;
     }
-    //bounce the virus off each wall
-      if (virus.x - virus.size / 2 < 0 && virus.xv < 0) {
-          virus.xv = -virus.xv;}
+    //bounce the viruses[i] off each wall
+      if (viruses[i].x - viruses[i].size / 2 < 0 && viruses[i].xv < 0) {
+          viruses[i].xv = -viruses[i].xv;}
   
-      if (virus.x + virus.size / 2 > canvas.width && virus.xv > 0) {
-          virus.xv = -virus.xv;}
+      if (viruses[i].x + viruses[i].size / 2 > canvas.width && viruses[i].xv > 0) {
+          viruses[i].xv = -viruses[i].xv;}
   
-      if (virus.y - virus.size / 2 < 0 && virus.yv < 0) {
-          virus.yv = -virus.yv;}
+      if (viruses[i].y - viruses[i].size / 2 < 0 && viruses[i].yv < 0) {
+          viruses[i].yv = -viruses[i].yv;}
   
-      if (virus.y + virus.size / 2 > canvas.height && virus.yv > 0) {
-          virus.yv = -virus.yv;}
+      if (viruses[i].y + viruses[i].size / 2 > canvas.height && viruses[i].yv > 0) {
+          viruses[i].yv = -viruses[i].yv;}
+    }
+
 
     // draw vaccine
     const drawVaccine = () => {
@@ -258,6 +269,7 @@ function update() {
       context.arc(virus.x+ virus.size / 2, virus.y + virus.size / 2, virus.size / 2, 0, Math.PI  * 2, false);
       context.stroke();
     }
+  
 
     //vaccine boundaries
     if (bounding) {
@@ -269,19 +281,26 @@ function update() {
 
     // checking for virus collision
     for(let i = 0; i < viruses.length; i++) {
-      if (distBetweenPoints(player.x, player.y, virus.x, virus.y) < (player.width + player.height) / 4 + virusSize / 2) {
+      if (distBetweenPoints(player.x, player.y, viruses[i].x, viruses[i].y) < (player.width + player.height) / 5 + virusSize / 2) {
         infection()
       }
     }
 
     // checking for vaccine collision
-    for(let i = 0; i < viruses.length; i++) {
-      if (distBetweenPoints(player.x, player.y, vaccine.x, vaccine.y) < (player.width + player.height) / 4 + vaccineSize / 2) {
-        infection()
-      }
+    if (distBetweenPoints(player.x, player.y, vaccine.x, vaccine.y) < (player.width + player.height) / 4 + vaccineSize / 2) {
+      vaccination()
+      player.vaccination++;     
     }
 
-    // draw score
+
+    // checking for hand sanitizer collision
+    // for(let i = 0; i < handSanitizers.length; i++) {
+    //   if (distBetweenPoints(handSanitizers.x, handSanitizers.y, virus.x, virus.y) < player.r / 8 + virusSize / 2) {
+    //     infection()
+    //   }
+    // }
+
+
   }
   
     
